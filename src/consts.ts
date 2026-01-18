b1e2c8912fb5724913abffd9b481ae7d7f5f56cd
Andrew Chen Wang <acwangpython@gmail.com>
2026-01-17 22:35:45 -0500

    fix: Move AGENTS.md file to content level?

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 // Website metadata
-export const SITE_URL: string = "https://blog.andrewcwang.com";
+export const SITE_URL: string = "https://andrewcwang.com";
 export const SITE_TITLE: string = "Andrew C Wang's Blog";
 export const SITE_DESCRIPTION: string = "Personal Blog";
 
